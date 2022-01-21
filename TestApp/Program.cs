579f25b276090a9c74e95a25db091041ce691b51
Renato Golia <renato.golia@insightarchitectures.com>
2022-01-21 14:12:32 +0100

    First commit

---
+++ TestApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Amazon;
+using Amazon.Extensions.NETCore.Setup;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+var aws = new AWSOptions {Region = RegionEndpoint.EUWest1 };
+
+var builder = new ConfigurationBuilder();
+builder.AddSystemsManager("/ParameterStoreReload/TestApp", aws, TimeSpan.FromSeconds(1));
+
+var configuration = builder.Build();
+
+var services = new ServiceCollection();
+
+services.AddOptions();
+
+services.AddTransient<Service>();
+services.Configure<ServiceOptions>(configuration);
+
+var sp = services.BuildServiceProvider();
+
+while (true)
+{
+    var svc = sp.GetRequiredService<Service>();
+
+    svc.PrintValue();
+
+    await Task.Delay(TimeSpan.FromMilliseconds(500));
+}
+
+public class Service
+{
+    private readonly IOptionsSnapshot<ServiceOptions> _options;
+
+    public Service(IOptionsSnapshot<ServiceOptions> options)
+    {
+        _options = options ?? throw new ArgumentNullException(nameof(options));
+    }
+
+    public void PrintValue()
+    {
+        Console.WriteLine($"{Guid.NewGuid():D}: {_options.Value.Value}");
+    }
+}
+
+public class ServiceOptions
+{
+    public int Value { get; set; }
+}
